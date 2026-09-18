6603829b19d1c46fb68da1ef891c39613143519d
Luiz Jurazek <luzjurazek@gmail.com>
2026-09-18 12:24:00 -0300

    fix: update manifest description and icon paths

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,19 +4,19 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Nomos",
     short_name: "Nomos",
-    description: "Descrição da minha aplicação PWA",
+    description: "Aplicação de finança",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/logo.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/logo.png",
         sizes: "512x512",
         type: "image/png",
       },
